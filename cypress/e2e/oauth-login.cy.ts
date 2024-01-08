65410d20f9f5eee6e7f50f4c4099023b768a06a5
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-08 23:20:22 +0100

    E2E Tests

--- cypress/e2e/oauth-login.cy.ts
+++ cypress/e2e/oauth-login.cy.ts
@@ -1,16 +1,25 @@
 describe('google oauth login', () => {
 	it('passes', () => {
-			cy.visit("https://localhost:3000")
+		cy.visit("https://localhost:3000")
+		cy.request({
+			method: 'POST',
+			url: 'https://www.googleapis.com/oauth2/v4/token',
+			body: {
+				grant_type: 'refresh_token',
+				client_id: Cypress.env('GOOGLE_CLIENT_ID'),
+				client_secret: Cypress.env('GOOGLE_CLIENT_SECRET'),
+				refresh_token: Cypress.env('GOOGLE_REFRESH_TOKEN'),
+			}
+		}).then(({ body }) => {
+			const { access_token } = body;
 			cy.request({
 				method: 'POST',
-				url: `${Cypress.env('API_ENDPOINT')}/auth/google-login`,
+				url: `${Cypress.env('API_URL')}/auth/google-login`,
 				body: {
-					accessToken: `${Cypress.env('GOOGLE_ACCESS_TOKEN')}`
+					accessToken: access_token
 				}
-			}).then(({ body }) => {
-				cy.reload();
 			});
-			
-		
-	})
-})
+		});
+		cy.reload();
+	});
+});
