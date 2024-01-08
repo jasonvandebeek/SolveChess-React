2b7a367d06a39ada8bea4fa813346ad3e815b491
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-08 13:08:38 +0100

    Dockerfile

--- cypress/e2e/oauth-login.cy.ts
+++ cypress/e2e/oauth-login.cy.ts
@@ -1,5 +1,16 @@
 describe('google oauth login', () => {
 	it('passes', () => {
-		cy.visit("https://localhost:3000/login");
+			cy.visit("https://localhost:3000")
+			cy.request({
+				method: 'POST',
+				url: `${Cypress.env('API_ENDPOINT')}/auth/google-login`,
+				body: {
+					accessToken: `${Cypress.env('GOOGLE_ACCESS_TOKEN')}`
+				}
+			}).then(({ body }) => {
+				cy.reload();
+			});
+			
+		
 	})
 })
