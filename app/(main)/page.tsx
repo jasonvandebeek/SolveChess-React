9bb3d28b4a58f7609424d00a49f7d03e45da3e7b
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-09 19:01:24 +0100

    E2E Tests

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -35,7 +35,7 @@ export default function Page() {
                 {state === 0 ? (
                     <div className="flex flex-col items-center mt-[-3rem] select-none">
                         <span className={`text-text font-extrabold font-montserrat-alt text-[5rem] drop-shadow-large ${wait && 'animate-fall-down-right-delayed'}`}>SOLVECHESS</span>
-                        <Button className={`px-[3rem] text-[1.5rem] ${wait && 'animate-fall-down-left'}`} onClick={handlePlayButtonClick}>Play</Button>
+                        <Button id="play-button" className={`px-[3rem] text-[1.5rem] ${wait && 'animate-fall-down-left'}`} onClick={handlePlayButtonClick}>Play</Button>
                     </div>
                 ) : (
                     <>
@@ -43,7 +43,7 @@ export default function Page() {
                         <div className="flex flex-row gap-[4rem]">
                             <GameCard title="Queue Up" className="animate-bounce-up-1"/>
                             <a href="/create-game/ai"><GameCard title="Play AI" className="animate-bounce-up-2"/></a>
-                            <a href="/create-game/player"><GameCard title="Create Game" className="animate-bounce-up-3"/></a>
+                            <a id="create-game-player" href="/create-game/player"><GameCard title="Create Game" className="animate-bounce-up-3"/></a>
                         </div>
                     </>
                 )}
