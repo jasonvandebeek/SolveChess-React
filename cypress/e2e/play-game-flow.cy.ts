ffbca71cc50e0cad4d6b26daf07f795eb9361643
jasonvandebeek <jasonvandebeek@gmail.com>
2024-01-18 00:10:56 +0100

    ci: jenkinsfile fix?

--- cypress/e2e/play-game-flow.cy.ts
+++ cypress/e2e/play-game-flow.cy.ts
@@ -2,7 +2,7 @@ import '../support/commands'
 
 describe('play move', () => {
 	it('passes', () => {
-		cy.visit(Cypress.env('HOST_URL'));
+		cy.visit(Cypress.env('TESTING_URL'));
 		cy.loginWithGoogleOauth();
 		cy.wait(2000);
 		cy.get("#play-button").click();
